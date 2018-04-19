616c994f8e738dd07c1bedfe2f9dd06889bbcd7d
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-19 13:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+  function destructivelyAppendKitten(name){
+    
+  }
+
+  function destructivelyPrependKitten(name){
+    
+  }
+
+  function destructivelyRemoveLastKitten() {
+    
+  }
+
+  function destructivelyRemoveFirstKitten () {
+    
+  }
+
+  function appendKitten(name) {
+    
+  }
+  
+  function prependKitten(name){
+    
+  }
+  
+  function removeLastKitten() {
+    
+  }
+
+  function removeFirstKitten() {
+    
+  }
